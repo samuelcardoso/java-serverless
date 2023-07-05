d76989050e8f762238b294a3836f5ffee20c0141
Samuel Cardoso <samuelparacontato@gmail.com>
2023-07-05 13:52:16 -0300

    feat:adding-spring-sample

---
+++ serverless-spring/src/main/java/org/sample/spring/springlambda/SpringlambdaApplication.java
@@ -0,0 +1,13 @@
+package org.sample.spring.springlambda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringlambdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringlambdaApplication.class, args);
+	}
+
+}
---
+++ serverless-spring/src/test/java/org/sample/spring/springlambda/SpringlambdaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sample.spring.springlambda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringlambdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
